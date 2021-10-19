5a447b078a4cda2cefcb53421d18a5ab758abcd5
Anderson Freitas <andersonfreitas.tec@gmail.com>
2021-10-19 08:24:14 -0300

    feat: add public ip search

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Generate will return the line command app ready to be executed.
 func Generate() *cli.App {
@@ -8,5 +14,32 @@ func Generate() *cli.App {
 	app.Name = "Command Line Application"
 	app.Usage = "Search IPs and Server names on internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Search IPs and Server names on internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: searchIps,
+		},
+	}
+
 	return app
+}
+
+func searchIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
